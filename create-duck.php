c7ffaabfb3e29d26a67f3a7c262e3e772359c8d5
Ashley Miles <agmiles@nic.edu>
2024-03-17 20:50:59 -0700

    Assignment 9.1 Finished

--- create-duck.php
+++ create-duck.php
@@ -2,40 +2,100 @@
 
 if (isset($_POST['submit'])) {
 
-    echo "post worked";
-
+    // create errors = array
     $errors = array(
         "name" => "",
         "favorite_foods" => "",
-        "bio" => ","
+        "bio" => "",
+        "img_src" => "",
     );
 
+    // get POST info
     $name = htmlspecialchars($_POST['name']);
     $favorite_foods = htmlspecialchars($_POST['favorite_foods']);
     $bio = htmlspecialchars($_POST['bio']);
+    $img_src = $_FILES["img_src"]["name"];
 
-    if(empty($name)) {
+    if (empty($name)) {
         $errors['name'] = "A name is required.";
     } else {
-        if
-
+        if (!preg_match('/^[a-z\s]+$/i', $name)) {
+            // echo "there is a name";
+            $errors["name"] = "illegal characters";
+        }
     }
 
-    if (preg_match('/^[a-z\s]+$/i', $name)) {
-        // echo "the name is formatted correctly";
+    if (empty($favorite_foods)) {
+        // if the name is empty
+        $errors['favorite_foods'] = "A food item is required";
     } else {
-        $errors["name"] = "The name has illegal characters";
+        if (!preg_match('/^[a-z\s]+$/i', $name)) {
+            // echo there is a name
+            $errors["favorite-foods"] = "illegal characters";
+        }
     }
 
-    if (preg_match('/^[a-z,\s]+$/i', $name)) {
-        // echo "the name is formatted correctly";
+    if (empty($bio)) {
+        $errors['bio'] = "A bio is required.";
+    }
+
+    // Handle file upload target directory
+    $target_dir = "./assets/images/";
+
+    // Target uploaded image file
+    $image_file = $target_dir . basename($_FILES["img_src"]["name"]);
+
+    // Get uploaded file extension so we can test to make sure it's an image
+    $image_file_type = strtolower(pathinfo($image_file, PATHINFO_EXTENSION));
+
+    // Test image for errors
+    // image exists
+    if (empty($img_src)) {
+        $errors["img_src"] = "An image is required.";
     } else {
-        $errors["favorite_foods"] = "Favorite foods must be separated by a comma.";
+
+        // Check that the image file is an actual image
+
+        $size_check = @getimagesize($_FILES["img_src"]["tmp_name"]);
+        $file_size = $_FILES["img_src"]["size"];
+        if (!$size_check) {
+            $errors["img_src"] = "File is not an image.";
+        }
+
+        // file size
+
+        else if ($file_size > 500000) {
+            $errors["img_src"] = "File cannot be larger than 500kb";
+        }
+
+        // file type (if it's an image)
+        else if (
+            $image_file_type != "jpg"
+            && $image_file_type != "png"
+            && $image_file_type != "jpeg"
+            && $image_file_type != "gif"
+            && $image_file_type != "webp"
+            && $image_file_type != "webp"
+        ) {
+            $errors["img_src"] = "Sorry, only JPG, JPEG, PNG, GIF, or WEBP files are allowed.";
+        }
+
+        // check if file already exists
+        else if (move_uploaded_file($_FILES["img_src"]['tmp_name'], $image_file)) {
+        } else {
+            $errors["img_src"] = "Sorrym there weas an error uploading your file.";
+        }
     }
 
-    print_r($errors);
+    if (!array_filter($errors)) {
+
+        require('./config/db.php');
+
+        $sql = "INSERT INTO ducks (name, favorite_foods, bio, img_src) VALUES ($name, $favorite_foods, $bio, $img_src)";
+
+        mysqli_query($conn, $sql);
+    }
 }
-
 
 ?>
 
@@ -49,27 +109,46 @@ include('./components/head.php');
     include('./components/nav.php');
     ?>
 
-    <section class="create-form">
-        <div class="container flex">
-            <form action="./create-duck.php" id="create-duck" method="POST">
+    <section class="create-form flex">
+        <div class="duck-container flex">
+            <form action="./create-duck.php" id="create-duck" class="form-info flex" enctype="multipart/form-data" method="POST">
                 <div class="form-intro">
                     <h1>Want a new duck?</h1>
-                    <p>Fill out the gotm to add a new duck!</p>
+                    <p>Fill out the form to add a new duck!</p>
                 </div>
-                <div class="input-group">
+                <div class="input-group flex">
                     <label for="name">Duck's Name</label>
+                    <?php
+                    if (isset($errors['name'])) {
+                        echo "<div class = 'error'>" . $errors["name"] . "</div>";
+                    }
+                    ?>
                     <input type="text" id="name" name="name" placeholder="Daffy">
                 </div>
-                <div class="input-group">
+                <div class="input-group flex">
                     <label for="foods">Favorite Foods (Separate multiple with comma)</label>
+                    <?php
+                    if (isset($errors['favorite_foods'])) {
+                        echo "<div class='error'>" . $errors["favorite_foods"] . "</div>";
+                    }
+                    ?>
                     <input type="text" id="favorite_foods" name="favorite_foods" placeholder="Eggs, ham, cheese">
                 </div>
-                <div class="input-group">
+                <div class="input-group flex">
+                    <label for="img_src">Duck's Picture</label>
+                    <input type="file" id="image" name="img_src">
+                </div>
+                <div class="input-group flex">
                     <label for="bio">Biography</label>
-                    <input type="text" id="bio" name="bio" rows="10" placeholder="Small duck who grew up in the big city.">
-                    <div class="input-group">
-<button></button>
-                    </div>
+                    <?php
+                    if (isset($errors['bio'])) {
+                        echo "<div class='error'>" . $errors["bio"] . "</div>";
+                    }
+                    ?>
+                    <input type="text" id="bio" name="bio" rows="10" placeholder="Tell us about your duck!">
+                </div>
+                <div class="input-group">
+                    <input type="submit" id="submit" value="Submit">
                 </div>
             </form>
         </div>
--- profile.php
+++ profile.php
@@ -1,4 +1,37 @@
 <?php
+
+$duck_is_live = false;
+require('./config/db.php');
+
+if (isset($_POST['delete'])) {
+
+    $id_to_delete = (int)$_POST['id_to_delete'];
+
+    $delete_sql = "DELETE FROM ducks WHERE id=$id_to_delete";
+
+    mysqli_query($conn, $delete_sql);
+}
+
+if (isset($_GET['id'])) {
+    // Assign a variable to the ID
+    $id = htmlspecialchars($_GET['id']);
+
+    // Create a query to select the intended duck from the db
+    $sql = "SELECT id, name, favorite_foods, bio, img_src FROM ducks WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+
+    $duck = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+    if (isset($duck['id'])) {
+        $duck_is_live = "true";
+    }
+}
+?>
+
+<?php
 $page_title = "Duck Profile";
 include('./components/head.php');
 ?>
@@ -7,15 +40,27 @@ include('./components/head.php');
     <?php
     include('./components/nav.php');
     ?>
+    <?php if ($duck_is_live) : ?>
+        <section class="flex profile">
+            <div class="profile-picture">
+                <img src="<?php echo $duck['img_src']; ?>" alt="duck.">
+            </div>
+            <div class="flex duck-info">
+                <h2><?php echo $duck['name']; ?></h2>
+                <h3>Favorite Foods:</h3>
+                <p><?php echo $duck['favorite_foods']; ?></p>
+                <p><?php echo $duck['bio']; ?></p>
+            </div>
+            <form action="./profile.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $duck['id']; ?>">
+                <input type="submit" name="delete" value="Delete Duck">
+            </form>
+        </section>
+    <?php else : ?>
 
-    <section class="flex profile">
-        <div class="profile-picture">
-            <img src="./assets/images/duck-in-pond.jpg" alt="duck in a pond">
-        </div>
-        <div class="flex duck-info">
-            <h2>Name</h2>
-            <h3>Favorite Foods:</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deserunt facere cum numquam. Obcaecati qui quae voluptates, consequuntur voluptatum impedit nostrum necessitatibus eveniet asperiores minima cupiditate saepe illum repudiandae quibusdam?</p>
-        </div>
-    </section>
+        <section class="no-duck">
+            <h1>Sorry this duck does not exist.</h1>
+        </section>
+
+    <?php endif ?>
 </body>
